8ef9fe63c3afd02941a8a88f6697f30cc47fcfc6
Julio Berina <julioberina@gmail.com>
2023-02-25 20:08:05 -0800

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AnnouncementsComponent } from './announcements/announcements.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AnnouncementsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
